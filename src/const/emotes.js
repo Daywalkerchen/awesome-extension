700b76679fd29996ab7822e8d74d306948a25f53
Carlotta Uhlenbusch <carlotta.uhlenbusch@flavia-it.de>
2022-12-16 18:37:19 +0100

    :sparkles: Add new emotes :pressf: :popcat: :bongocat: :charmander: :noted: :vibebunny: :discocat: :kekw:

--- src/const/emotes.js
+++ src/const/emotes.js
@@ -26,5 +26,29 @@ export const EMOTES = [
     }, {
         tag: ":amongus:",
         url: "https://cdn.betterttv.net/emote/632369e78d546373770763b7/3x"
+    }, {
+        tag: ":pressf:",
+        url: "https://cdn.betterttv.net/emote/5c857788f779543bcdf37124/3x"
+    }, {
+        tag: ":popcat:",
+        url: "https://cdn.betterttv.net/emote/5fa8f232eca18f6455c2b2e1/3x"
+    }, {
+        tag: ":bongocat:",
+        url: "https://cdn.betterttv.net/emote/5ba6d5ba6ee0c23989d52b10/3x"
+    }, {
+        tag: ":charmander:",
+        url: "https://cdn.betterttv.net/emote/562b9101a6646e202bcc5447/3x"
+    }, {
+        tag: ":noted:",
+        url: "https://cdn.betterttv.net/emote/61a142a9b50549e7e50108a7/3x"
+    }, {
+        tag: ":vibebunny:",
+        url: "https://cdn.betterttv.net/emote/6228bfe606fd6a9f5be69c39/3x"
+    }, {
+        tag: ":discocat:",
+        url: "https://cdn.betterttv.net/emote/5fd03d2acac2fb4621e5690a/3x"
+    }, {
+        tag: ":kekw:",
+        url: "https://cdn.betterttv.net/emote/5e9c6c187e090362f8b0b9e8/3x"
     }
 ];
