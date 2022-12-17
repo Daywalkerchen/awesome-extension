8eecb9ef5722ac6754b48c77407a14f6d36b1aae
Carlotta Uhlenbusch <carlotta.uhlenbusch@flavia-it.de>
2022-12-17 03:15:36 +0100

    :fire: :lipstick: Remove old storage and add a lot of scss

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,14 +1,52 @@
-import React from "react";
-import "./Popup.css";
+import React, { useEffect, useState } from 'react';
+import './Popup.scss';
 
 const Popup = () => {
-    return (
-      <div className="App">
-          <div>
-              <p>Replace Emotes</p>
-          </div>
+  const [enableAlternates, setEnableAlternates] = useState(null);
+
+  // Restores select box and checkbox state using the preferences
+  // stored in chrome.storage.
+  useEffect(() => {
+    chrome.storage.sync.get({ enableAlternates }, items => {
+      setEnableAlternates(items.enableAlternates);
+      console.log(items.enableAlternates);
+    });
+  }, []);
+
+  // Saves options to chrome.storage
+  const saveOptions = () => {
+    chrome.storage.sync.set({ enableAlternates: enableAlternates }, () => {
+      console.log('enableAlternates set to ' + enableAlternates);
+    });
+  };
+
+  const handleChange = (e) => {
+    setEnableAlternates(e.target.checked);
+  };
+
+  return (
+    <div className="App">
+      <h2>awesome-extension</h2>
+      <div className="settings-wrapper">
+        <input
+          className="checkbox"
+          type="checkbox"
+          checked={!!enableAlternates}
+          onChange={handleChange}
+        />
+        <label>
+          Enable alternate replacement tags
+        </label>
       </div>
-    );
+      <div id="status"></div>
+      <button
+        className="button"
+        onClick={saveOptions}
+      >
+        Save
+      </button>
+    </div>
+  );
 };
 
 export default Popup;
