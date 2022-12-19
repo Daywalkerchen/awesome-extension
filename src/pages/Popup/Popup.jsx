5b7c0c9a25bd68ce136503fbbca3a741b6aae392
Carlotta Uhlenbusch <carlotta.uhlenbusch@flavia-it.de>
2022-12-19 19:50:59 +0100

    :sparkles: Add garbage copy-past emote-picker

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './Popup.scss';
 import Header from './header/Header';
 import Settings from './settings/Settings';
+import EmotePicker from './emote-picker/EmotePicker';
 
 const Popup = () => (
   <div className="popup">
     <Header />
     <Settings />
+    <EmotePicker />
   </div>
 );
 
