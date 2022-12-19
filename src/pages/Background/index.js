b8459806c07d4493109ddf6c45a288388437b7f8
Carlotta Uhlenbusch <carlotta.uhlenbusch@flavia-it.de>
2022-12-19 18:54:44 +0100

    :technologist: Improve readability

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,11 +1,7 @@
 console.log('[BACKGROUND] Script loaded successfully');
 chrome.storage.onChanged.addListener((changes) => {
-  if (!changes.replacedPlaceholder || !changes.replacedPlaceholder.newValue) {
-    chrome.action.setBadgeText({ text: '' });
-    chrome.action.setBadgeBackgroundColor({ color: 'gray' });
-  } else {
-    // let the user know by how much the overall fun has been increased
-    chrome.action.setBadgeText({ text: changes.replacedPlaceholder.newValue.toString() });
-    chrome.action.setBadgeBackgroundColor({ color: 'gray' });
-  }
+  const badgeText = changes.replacedPlaceholder?.newValue?.toString() || '';
+
+  chrome.action.setBadgeBackgroundColor({ color: 'gray' });
+  chrome.action.setBadgeText({ text: badgeText });
 });
