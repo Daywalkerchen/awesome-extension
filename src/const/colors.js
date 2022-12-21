0d1842449f56f4cc3d6a84027fba7821df5f4d39
Carlotta Uhlenbusch <carlotta.uhlenbusch@flavia-it.de>
2022-12-21 15:51:29 +0100

    :art: Move colors to const and format code

---
+++ src/const/colors.js
@@ -0,0 +1,1 @@
+export const COLORS = ['slategray', 'gray', 'silver', '#0070dd', '#a335ee', '#ff8000', '#e6cc80'];
--- src/pages/Background/badge.js
+++ src/pages/Background/badge.js
@@ -1,10 +1,15 @@
-const COLORS = ['slategray', 'gray', 'silver', '#0070dd', '#a335ee', '#ff8000', '#e6cc80'];
+// region Imports
+import { COLORS } from '../../const/colors';
+// endregion
+
 const FORMATTER = Intl.NumberFormat('en', { notation: 'compact' });
 
 chrome.storage.onChanged.addListener((changes) => {
   const replaceCounter = changes.replacedPlaceholder?.newValue || 0;
+
   const color = COLORS[Math.min(Math.trunc(replaceCounter).toString().length, COLORS.length) - 1];
   const text = FORMATTER.format(replaceCounter);
+
   chrome.action.setBadgeBackgroundColor({ color });
   chrome.action.setBadgeText({ text });
 });
