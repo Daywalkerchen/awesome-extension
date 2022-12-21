977a2510c5af3803187e66bd3aba975e4dde87e7
Carlotta Uhlenbusch <carlotta.uhlenbusch@flavia-it.de>
2022-12-21 16:02:54 +0100

    :lipstick: Add exotic color

--- src/const/colors.js
+++ src/const/colors.js
@@ -1,1 +1,10 @@
-export const COLORS = ['slategray', 'gray', 'silver', '#0070dd', '#a335ee', '#ff8000', '#e6cc80'];
+export const COLORS = [
+  '#708090', // Common #1
+  '#808080', // Common #2
+  '#C0C0C0', // Common #3
+  '#0070dd', // Rare
+  '#a335ee', // Epic
+  '#ff8000', // Legendary
+  '#e6cc80', // Mythic
+  '#d31319', // Exotic
+];
