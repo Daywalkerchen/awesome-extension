f68c230b481e40fc3034aa49ca642f43de3d5c44
Ceco <ceco@konze.me>
2022-12-29 01:01:33 +0100

    Added emote-popup to rocket chat text field. - Emotes will be directly written to the chat text field instead of copying them first
    This is a combination of 5 commits.
    - react injection test
    - add reactjs popup
    - ref emote-popup component to shared component folder
    - add emote-picker-for-chat-input react component
    - added logic for popup injection on rocket chat

--- src/components/emote-picker/emote/Emote.jsx
+++ src/components/emote-picker/emote/Emote.jsx
@@ -3,14 +3,11 @@ import React from 'react';
 import './emote.scss';
 // endregion
 
-const Emote = ({ emote }) => {
+const Emote = ({ emote, onClick }) => {
   const tag = emote.tags[0];
-  const onClick = async () => {
-    await navigator.clipboard.writeText(tag);
-  };
 
   return (
-    <div onClick={onClick} className="emote">
+    <div onClick={() => onClick(tag)} className="emote">
       <img src={emote.url} title={tag} alt={tag} />
     </div>
   );
