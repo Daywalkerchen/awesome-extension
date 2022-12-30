d716309d001cb7014f5945209e28668b0b2a6ea4
Ceco <ceco@konze.me>
2022-12-30 15:39:15 +0100

    resolve review comments

--- src/components/emote-picker-on-input/emotePickerOnInput.jsx
+++ src/components/emote-picker-on-input/emotePickerOnInput.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
-import './emotePickerOnInput.scss';
 
 import EmotePicker from '../emote-picker/EmotePicker';
 
 const EmotePickerOnInput = ({ inputElem }) => {
-  // todo make __perfect__ 28px icon
   const src = chrome.runtime.getURL('icon-34.png');
 
   const onClick = (tag) => {
@@ -15,9 +13,7 @@ const EmotePickerOnInput = ({ inputElem }) => {
 
   return (
     <Popup className="input-emote-picker" trigger={<img src={src} alt="better emotes" />} position="top right">
-      <div>
-        <EmotePicker onClick={onClick} />
-      </div>
+      <EmotePicker onClick={onClick} />
     </Popup>
   );
 };
