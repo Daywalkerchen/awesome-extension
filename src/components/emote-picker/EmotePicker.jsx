c45b3a0a4d566425798baa76c6da89aef77d16ea
Alexander Berger <abseboco@gmail.com>
2023-01-23 20:39:50 +0100

    :sparkles: Add keywords to EMOTE constant

--- src/components/emote-picker/EmotePicker.jsx
+++ src/components/emote-picker/EmotePicker.jsx
@@ -3,8 +3,41 @@ import { EMOTES } from '../../const/emotes';
 import Emote from './emote/Emote';
 import './emotePicker.scss';
 
+/**
+ * @param {string[] | undefined} arrayOfStrings
+ * @param {string} lowerSearch
+ * @returns {boolean}
+ */
+const searchInArray = (arrayOfStrings, lowerSearch) => {
+  if (!arrayOfStrings) {
+    return false;
+  }
+
+  return arrayOfStrings.map((element) => element.toLowerCase()).some((element) => element.includes(lowerSearch));
+};
+
+/**
+ * @param {string | undefined} searchString
+ * @returns { {
+ *   tags: string[];
+ *   url: string;
+ *   keywords: string[];
+ * } }
+ */
+const searchEmotes = (searchString) => {
+  const lowerSearch = searchString?.toLowerCase();
+
+  return EMOTES.filter((emote) => {
+    if (!lowerSearch) {
+      return true;
+    }
+
+    return searchInArray(emote.tags, lowerSearch) || searchInArray(emote.keywords, lowerSearch);
+  });
+};
+
 const EmotePicker = ({ searchString, onClick }) => {
-  const filteredList = EMOTES.filter((emote) => !searchString || emote.tags[0].toLowerCase().includes(searchString?.toLowerCase()));
+  const filteredList = searchEmotes(searchString);
 
   return (
     <div className="emote-picker">
--- src/const/emotes.js
+++ src/const/emotes.js
@@ -4,121 +4,151 @@ export const EMOTES = [
   {
     tags: [':blobdance:', 'c8731aa4-b90f-4482 84c2-65b9be019cd5'], // this random id may be a random person's name idk
     url: `${emotesBasePath}/blobdance.gif`,
+    keywords: ['blob'],
   },
   {
     tags: [':birbrave:'],
     url: `${emotesBasePath}/birbrave.gif`,
+    keywords: ['animal'],
   },
   {
     tags: [':catjam:'],
     url: `${emotesBasePath}/catjam.gif`,
+    keywords: ['animal'],
   },
   {
     tags: [':hype:'],
     url: `${emotesBasePath}/hype.gif`,
+    keywords: ['animal'],
   },
   {
     tags: [':discocat:'],
     url: `${emotesBasePath}/discocat.gif`,
+    keywords: ['animal'],
   },
   {
     tags: [':vibebunny:', ':vibe:'],
     url: `${emotesBasePath}/vibebunny.gif`,
+    keywords: ['animal'],
   },
   {
     tags: [':bongocat:'],
     url: `${emotesBasePath}/bongocat.gif`,
+    keywords: ['animal'],
   },
   {
     tags: [':popcat:'],
     url: `${emotesBasePath}/popcat.gif`,
+    keywords: ['animal'],
   },
   {
     tags: [':typingcat:'],
     url: `${emotesBasePath}/typingcat.gif`,
+    keywords: ['animal'],
   },
   {
     tags: [':thisisfine:'],
     url: `${emotesBasePath}/thisisfine.gif`,
+    keywords: ['animal'],
   },
   {
     tags: [':surprised-pikachu:'],
     url: `${emotesBasePath}/surprised-pikachu.gif`,
+    keywords: ['pokemon'],
   },
   {
     tags: [':confused_dog:'],
     url: `${emotesBasePath}/confused_dog.gif`,
+    keywords: ['animal'],
   },
   {
     tags: [':suss:'],
     url: `${emotesBasePath}/suss.gif`,
+    keywords: ['animal'],
   },
   {
     tags: [':amongus:'],
     url: `${emotesBasePath}/amongus.gif`,
+    keywords: ['sus'],
   },
   {
     tags: [':pressf:'],
     url: `${emotesBasePath}/pressf.gif`,
+    keywords: ['keyboard'],
   },
   {
     tags: [':squirtle:', ':schiggy:'],
     url: `${emotesBasePath}/squirtle.gif`,
+    keywords: ['pokemon'],
   },
   {
     tags: [':bulbasaur:', ':bisasam:', ':awww:'],
     url: `${emotesBasePath}/bulbasaur.gif`,
+    keywords: ['pokemon'],
   },
   {
     tags: [':charmander:'],
     url: `${emotesBasePath}/charmander.gif`,
+    keywords: ['pokemon'],
   },
   {
     tags: [':weewoo:'],
     url: `${emotesBasePath}/weewoo.gif`,
+    keywords: ['alarm'],
   },
   {
     tags: [':noted:'],
     url: `${emotesBasePath}/noted.gif`,
+    keywords: ['alarm'],
   },
   {
     tags: [':sadge:'],
     url: `${emotesBasePath}/sadge.gif`,
+    keywords: ['pepe'],
   },
   {
     tags: [':shypeepo:'],
     url: `${emotesBasePath}/shypeepo.gif`,
+    keywords: ['pepe'],
   },
   {
     tags: [':poggers:'],
     url: `${emotesBasePath}/poggers.png`,
+    keywords: ['pepe'],
   },
   {
     tags: [':kappa:'],
     url: `${emotesBasePath}/kappa.gif`,
+    keywords: ['human'],
   },
   {
     tags: [':kekw:'],
     url: `${emotesBasePath}/kekw.gif`,
+    keywords: ['human'],
   },
   {
     tags: [':couldyounot:'],
     url: `${emotesBasePath}/couldyounot.gif`,
+    keywords: ['human'],
   },
   {
     tags: [':derp:', ':menue:'],
     url: `${emotesBasePath}/derp.png`,
+    keywords: [],
   },
   {
     tags: [':walkingduck:', ':duck:'],
     url: `${emotesBasePath}/walkingduck.gif`,
+    keywords: ['animal'],
   },
   {
     tags: [':blobsaber:'],
     url: `${emotesBasePath}/blobsaber.gif`,
+    keywords: ['blob'],
   },
   {
     tags: [':technoviking:'],
     url: `${emotesBasePath}/technoviking.gif`,
+    keywords: [],
   },
 ];
