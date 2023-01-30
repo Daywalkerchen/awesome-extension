6210d446b686da02ea8b9c38b137b0ca9a98d181
Alexander Berger <abseboco@gmail.com>
2023-01-30 21:29:45 +0100

    :recycle: Add useSyncStorage hook

--- src/features/Options/settings/Settings.jsx
+++ src/features/Options/settings/Settings.jsx
@@ -1,42 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useSyncStorage } from '../../../hooks/storage';
 import './settings.scss';
 
 const Settings = () => {
-  const [enableAlternates, setEnableAlternates] = useState(null);
-
-  // Restores select box and checkbox state using the preferences
-  // stored in chrome.storage.
-  useEffect(() => {
-    chrome.storage.sync.get({ enableAlternates }, (items) => {
-      setEnableAlternates(items.enableAlternates);
-      console.log(items.enableAlternates);
-    });
-  }, []);
+  const [enableAlternates, setEnableAlternates] = useSyncStorage('enableAlternates', false);
+  const [replacedPlaceholder, setReplacedPlaceholder] = useSyncStorage('replacedPlaceholder', 0);
 
   // Sets the global replacement counter to 0
   const resetCounter = () => {
-    chrome.storage.sync.set({ replacedPlaceholder: 0 }, () => {
-      console.log('resetted replacedPlaceholder');
-    });
+    setReplacedPlaceholder(0);
   };
 
   const handleChange = () => {
-    const newValue = !enableAlternates;
-
-    setEnableAlternates(newValue);
-
-    chrome.storage.sync.set({ enableAlternates: newValue }, () => {
-      console.log('enableAlternates set to ' + newValue);
-    });
+    setEnableAlternates(!enableAlternates);
   };
 
   return (
     <div className="settings">
       <input id="enable-alternatives" className="checkbox" type="checkbox" checked={!!enableAlternates} onChange={handleChange} />
-      <label for="enable-alternatives">Enable alternate replacement tags</label>
+      <label htmlFor="enable-alternatives">Enable alternate replacement tags</label>
       <div className="actions">
         <button className="button" onClick={resetCounter}>
-          Reset Count
+          Reset Count ({replacedPlaceholder})
         </button>
       </div>
     </div>
---
+++ src/hooks/storage.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+
+export const useSyncStorage = (key, defaultValue) => {
+  const [rawValue, setRawValue] = useState(null);
+
+  useEffect(() => {
+    const keys = {
+      [key]: defaultValue,
+    };
+    chrome.storage.sync.get(keys, (items) => {
+      setRawValue(items[key]);
+    });
+
+    const listener = (changes) => {
+      const change = changes[key];
+      if (!change) {
+        return;
+      }
+
+      const newValue = change.newValue ?? defaultValue;
+      setRawValue(newValue);
+    };
+    chrome.storage.onChanged.addListener(listener);
+    const cleanup = () => {
+      chrome.storage.onChanged.removeListener(listener);
+    };
+    return cleanup;
+  }, []);
+
+  const setValue = (newValue) => {
+    chrome.storage.sync.set({ [key]: newValue }, () => {
+      console.log('Set', key, 'to', newValue);
+      // state will be changed by the event listener
+      // this way we do not call setRawValue twice
+    });
+  };
+
+  return [rawValue, setValue];
+};
